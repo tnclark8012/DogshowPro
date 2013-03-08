f6f454bc41f4cb4042bb7a52af4e37e8c0ee8663
Taylor Clark <tnclark8012@hotmail.com>
2013-03-08 17:08:31 -0600

    added "show" URL. gitignore added
    http://dogshow-manager.appspot.com/show returns JSON from trin1jp.json.

--- DogshowServer/src/dev/tclark/dogshow/models/show/Ring.java
+++ DogshowServer/src/dev/tclark/dogshow/models/show/Ring.java
@@ -1,5 +1,21 @@
 package dev.tclark.dogshow.models.show;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
 public class Ring {
 
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	
+	@Persistent
+	private String title;
+	
+	
 }
