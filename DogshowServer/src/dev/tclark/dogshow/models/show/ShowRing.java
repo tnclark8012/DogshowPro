1276f094d7b1305370c12dd028468544614f5fbe
Taylor Clark <tnclark8012@hotmail.com>
2013-03-16 13:04:09 -0500

    Objectify + Jackson

--- DogshowServer/src/dev/tclark/dogshow/models/show/ShowRing.java
+++ DogshowServer/src/dev/tclark/dogshow/models/show/ShowRing.java
@@ -1,5 +1,28 @@
 package dev.tclark.dogshow.models.show;
 
-public class ShowRing {
+import java.util.Random;
 
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.googlecode.objectify.Key;
+
+@XmlRootElement(name="Ring")
+public class ShowRing {
+	@Id
+	private Long id;
+	@XmlElement
+	private int number;
+
+	private Key<ShowDay> day;
+
+	public ShowRing() {
+		this.number = (new Random().nextInt(13) + 1);
+	}
+
+	public void setShowDay(ShowDay day) {
+		
+		//this.day = new Key<ShowDay>(ShowDay.class, day.getId().longValue());
+	}
 }
