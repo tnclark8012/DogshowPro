11d3ae5fff0be05e0939aef0855e31e7e415a2fe
Taylor Clark <tnclark8012@hotmail.com>
2013-03-16 13:04:09 -0500

    Objectify + Jackson

--- DogshowServer/src/dev/tclark/dogshow/models/show/Show.java
+++ DogshowServer/src/dev/tclark/dogshow/models/show/Show.java
@@ -7,40 +7,65 @@ import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.google.appengine.api.datastore.Key;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 @PersistenceCapable
+@XmlRootElement
 public class Show {
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
 	private Key key;
-	
+
 	@Persistent
+	@XmlElement
 	private String showName;
-	
+
+	@XmlElement
 	@Persistent
 	private Date startDate;
-	
+
+	@XmlElement
 	@Persistent
 	private Date endDate;
-	
+
+	@XmlElement(name="Days")
 	@Persistent
 	List<ShowDay> showDays;
-	
-	public Show(String showName, Date startDate, Date endDate, List<ShowDay> showDays)
+
+	public Show()
 	{
+		startDate = new Date();
+		endDate = new Date();
+		showName = "Taylor's Dog Show";
+	}
+	public Show(String showName, Date startDate, Date endDate,
+			List<ShowDay> showDays) {
 		this.showName = showName;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.showDays = showDays;
 	}
-	
-	public String getShowName(){
+
+	public String getShowName() {
 		return showName;
 	}
-	
-	public List<ShowDay> getShowDays(){
+
+	public List<ShowDay> getShowDays() {
 		return showDays;
+	}
+
+	public Show(String showName, Date startDate, Date endDate, JSONObject json)
+	{
+//		JSONArray ringArray = json.getJSONArray("Rings");
+//		ShowDay day = new ShowDay(startDate);
+//		for( int i = 0; i < ringArray.length(); i++)
+//		{
+//			ringArray.get(i);
+//		}
+		//this(showName, startDate, endDate, );
 	}
 }
