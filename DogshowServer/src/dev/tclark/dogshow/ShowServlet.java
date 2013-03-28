507ed052b7772dc6627d50e5722b422447b4523f
Taylor Clark <tnclark8012@hotmail.com>
2013-03-28 12:12:03 -0500

    Added rest/shows request
    get upcoming shows for list

--- DogshowServer/src/dev/tclark/dogshow/ShowServlet.java
+++ DogshowServer/src/dev/tclark/dogshow/ShowServlet.java
@@ -1,28 +1,52 @@
 package dev.tclark.dogshow;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 import dev.tclark.dogshow.models.show.Show;
 import dev.tclark.dogshow.models.show.ShowDay;
 import dev.tclark.dogshow.models.show.ShowRing;
-@Path("/")
+@Path("shows")
 public class ShowServlet {
 		private static final long serialVersionUID = -9014811090548638110L;
 		private static final Logger log = Logger.getLogger(ShowServlet.class
 				.getName());
 		@GET
 		@Produces("application/json")
-		public ShowDay sayHello()
+		@Path("{id}")
+		public ShowDay getShowsById(@PathParam("id") long id)
 		{
 			ShowDay day = new ShowDay();
 			day.addShowRing(new ShowRing());
 			day.addShowRing(new ShowRing());
 			day.addShowRing(new ShowRing());			
 			return day;
+		}
+		@GET
+		@Produces("application/json")
+		public List<Show> getAllShows()
+		{
+			Date start;
+			Calendar cal = new GregorianCalendar(Locale.US);
+			start = cal.getTime();
+			cal.add(Calendar.DAY_OF_MONTH, 2);
+			Date end = cal.getTime();
+			List<Show> shows = new LinkedList<Show>();
+			shows.add(Show.createDummyInstance(0, "Trinity Dog Club Specialty"));
+			shows.add(Show.createDummyInstance(1,"Lone Star Classic"));
+			return shows;
 		}
 //
 //		@Override
