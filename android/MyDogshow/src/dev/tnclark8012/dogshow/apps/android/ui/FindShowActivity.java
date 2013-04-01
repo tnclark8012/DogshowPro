91dac9d61a3304016ab21f1a2bbbcd92a4314776
Taylor Clark <tnclark8012@hotmail.com>
2013-03-31 21:33:00 -0500

    merged master into Server, something went wrong with the FindShow source, so I copied from Android branch

--- android/MyDogshow/src/dev/tnclark8012/dogshow/apps/android/ui/FindShowActivity.java
+++ android/MyDogshow/src/dev/tnclark8012/dogshow/apps/android/ui/FindShowActivity.java
@@ -1,12 +1,26 @@
 package dev.tnclark8012.dogshow.apps.android.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
-public class FindShowActivity extends SimpleSinglePaneActivity{
-
+public class FindShowActivity extends SimpleSinglePaneActivity implements FindShowFragment.Callbacks{
+	private static final String TAG = FindShowActivity.class.getSimpleName();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
 	@Override
 	protected Fragment onCreatePane() {
-		return ;
+		return new FindShowFragment();
+	}
+	@Override
+	public void onShowSelected(long showId) {
+		Log.v(TAG, "selected show. id: " + showId);
+	}
+	@Override
+	public void onShowSynced() {
+
 	}
 
 }
