2ba6e18ca7a3742149b99bdb4660d75ee25d9d94
Taylor Clark <tnclark8012@hotmail.com>
2013-04-04 01:10:40 -0500

    breed rings pull and schedule

--- android/MyDogshow/src/dev/tnclark8012/dogshow/apps/android/sql/DogshowDatabase.java
+++ android/MyDogshow/src/dev/tnclark8012/dogshow/apps/android/sql/DogshowDatabase.java
@@ -123,34 +123,6 @@ public class DogshowDatabase extends SQLiteOpenHelper {
 //				+ System.currentTimeMillis()
 //				+ ")");
 
-		db.execSQL("INSERT INTO " + Tables.BREED_RINGS + "("
-				+ BreedRingsColumns.RING_BITCH_COUNT + ","
-				+ BreedRingsColumns.RING_BLOCK_START + ","
-				+ BreedRingsColumns.RING_BREED + ","
-				+ BreedRingsColumns.RING_BREED_COUNT + ","
-				+ BreedRingsColumns.RING_COUNT_AHEAD + ","
-				+ BreedRingsColumns.RING_DATE + ","
-				+ BreedRingsColumns.RING_DOG_COUNT + ","
-				+ BreedRingsColumns.RING_JUDGE + ","
-				+ BreedRingsColumns.RING_NUMBER+ ","
-				+ BreedRingsColumns.RING_SHOW_ID + ","
-				+ BreedRingsColumns.RING_SPECIAL_BITCH_COUNT + ","
-				+ BreedRingsColumns.RING_SPECIAL_DOG_COUNT + ")" + " VALUES ("
-				+ "2,"
-				+ (new GregorianCalendar().getTimeInMillis()+2*60*1000)
-				+ "\"Papillon\","
-				+ "10,"
-				+ "15,"
-				+ (today.getTimeInMillis()) +","
-				+ "5,"
-				+ "\"Dr. Steve Keating\","
-				+ "3,"
-				+ "TEST"
-				+ "2,"
-				+ "1"
-				+ ")");
-
-
 	}
 
 	@Override
