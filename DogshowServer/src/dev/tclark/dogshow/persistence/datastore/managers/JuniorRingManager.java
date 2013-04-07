456987849260190394ccbe7c7a070f12affe363a
Taylor Clark <tnclark8012@hotmail.com>
2013-04-06 19:12:56 -0500

    Added REST request for junior rings

--- DogshowServer/src/dev/tclark/dogshow/persistence/datastore/managers/JuniorRingManager.java
+++ DogshowServer/src/dev/tclark/dogshow/persistence/datastore/managers/JuniorRingManager.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 import dev.tclark.dogshow.persistence.datastore.JuniorRing;
 import dev.tclark.dogshow.persistence.datastore.accessors.JuniorRingAccessor;
+import dev.tnclark8012.dogshow.shared.DogshowEnums;
+import dev.tnclark8012.dogshow.shared.DogshowEnums.JuniorClass;
 
 public class JuniorRingManager {
 	public static void createJuniorRing(JuniorRing ring) {
 		JuniorRingAccessor.createJuniorRing(ring);
 	}
 
-	public static List<JuniorRing> getJuniorRingsByShowId(String showId) {
-		return JuniorRingAccessor.getJuniorRingsByShowId(showId);
+	public static List<JuniorRing> getAllJuniorRingsByShowId(String showId) {
+		return JuniorRingAccessor.getAllJuniorRingsByShowId(showId);
 	}
 
+	public static List<JuniorRing> getJuniorRingsByShowIdClassName(String showId, JuniorClass clazz) {
+		return JuniorRingAccessor.getAllJuniorRingsByShowIdClassName(showId, clazz.getPrimaryName());
+	}
+	
 	public static void deleteAllJuniorRings(boolean sure, boolean reallySure,
 			boolean positive, boolean knowConsequences, boolean stupid) {
 		JuniorRingAccessor.deleteAllJuniorRings(sure, reallySure, positive, knowConsequences, stupid);
--- DogshowShared/src/dev/tnclark8012/dogshow/shared/driver/BreedRingsResponse.java
+++
@@ -1,8 +0,0 @@
-package dev.tnclark8012.dogshow.shared.driver;
-
-import com.google.gson.annotations.SerializedName;
-
-public class BreedRingsResponse {
-	@SerializedName("breedRing")
-    public BreedRing[] breedRings;
-}
