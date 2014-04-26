3d890780b9cbf85f8d8e1aa54de702c95e897de3
Taylor Clark <tnclark8012@hotmail.com>
2014-04-25 20:38:03 -0700

    removed old App Engine server, crop-image library example

--- DogshowServer/src/dev/tnclark8012/dogshow/persistence/datastore/CustomKeyName.java
+++
@@ -1,5 +0,0 @@
-package dev.tnclark8012.dogshow.persistence.datastore;
-
-public interface CustomKeyName {
-	public String getKeyName();
-}
--- DogshowServer/src/dev/tnclark8012/dogshow/persistence/datastore/Storable.java
+++
@@ -1,7 +0,0 @@
-package dev.tnclark8012.dogshow.persistence.datastore;
-
-import com.google.appengine.api.datastore.Entity;
-
-public interface Storable {
-	public Entity toDatastoreEntity();
-}
