65b1e2f46c69784154f18e61962ae9ecf78fe8b1
Taylor Clark <tnclark8012@gmail.com>
2014-08-18 14:37:11 -0700

    Changed HttGetWithEntity
    changed entire impl

--- DogshowPro/myDogshow/src/main/java/dev/tnclark8012/apps/android/dogshow/sync/request/HttpGetWithEntity.java
+++ DogshowPro/myDogshow/src/main/java/dev/tnclark8012/apps/android/dogshow/sync/request/HttpGetWithEntity.java
@@ -1,14 +1,32 @@
 package dev.tnclark8012.apps.android.dogshow.sync.request;
 
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+
+import java.net.URI;
 
 /**
  * Created by Taylor on 8/9/2014.
  */
-public class HttpGetWithEntity extends HttpPost {
-    public static final String METHOD = "GET";
+public class HttpGetWithEntity extends HttpEntityEnclosingRequestBase {
+
+    public HttpGetWithEntity() {
+        super();
+    }
+
+    public HttpGetWithEntity(URI uri) {
+        super();
+        setURI(uri);
+    }
+
+    public HttpGetWithEntity(String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
+
     @Override
     public String getMethod() {
-        return METHOD;
+        return HttpGet.METHOD_NAME;
     }
 }
