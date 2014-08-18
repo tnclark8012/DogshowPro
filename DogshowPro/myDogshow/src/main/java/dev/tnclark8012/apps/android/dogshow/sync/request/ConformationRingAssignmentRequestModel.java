61b2027ef58ba444f015b95d534dde098371b18b
Taylor Clark <tnclark8012@gmail.com>
2014-08-18 14:38:19 -0700

    Model for ring assignment request

---
+++ DogshowPro/myDogshow/src/main/java/dev/tnclark8012/apps/android/dogshow/sync/request/ConformationRingAssignmentRequestModel.java
@@ -0,0 +1,11 @@
+package dev.tnclark8012.apps.android.dogshow.sync.request;
+
+import dev.tnclark8012.apps.android.dogshow.model.Dog;
+
+/**
+ * Created by taylorc on 8/18/2014.
+ */
+public class ConformationRingAssignmentRequestModel {
+    public Dog[] dogs;
+    public String showId;
+}
