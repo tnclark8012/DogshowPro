4e2d912a54ee1b4eab78d9fc78edd24ab7d9d4a2
Taylor Clark <tnclark8012@hotmail.com>
2015-05-31 03:43:57 -0700

    reworking directory structure, migrating slowly to reduce bloat

---
+++ Woof/app/src/androidTest/java/tnclark8012/dev/woof/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tnclark8012.dev.woof;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
